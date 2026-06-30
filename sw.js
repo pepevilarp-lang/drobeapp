f8f6c5efb9fe14e6319a1c64486c70cde351e83d
pepevilarp-lang <pepevilarp@gmail.com>
2026-06-30 10:00:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drobe-v30';
+const CACHE = 'drobe-v31';
 const SHELL = [
   './','./index.html','./styles.css','./app.js','./wardrobe3d.js',
   './lib/supabase.js','./manifest.webmanifest',
